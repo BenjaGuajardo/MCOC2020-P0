694690e18152c3c6b28c2b0e2d725d0b36d59efa
BenjaGuajardo <69161061+BenjaGuajardo@users.noreply.github.com>
2020-08-04 15:32:42 -0400

    Add files via upload

---
+++ hola.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 08:37:34 2020
+
+@author: benjaminguajardo
+"""
+
+nombre='Benja'
+dia=3
+
+print ('Hola '+nombre+' es el '+str(dia)+ ' de Agosto')
+print (f'Hola {nombre} es el {dia} de Agosto')
